5114b8ad63cf9325cf2599a0a49ec814ec73ba3d
Budleigh Salterton <cz@scramjet.org>
2021-08-28 22:04:53 +0000

    Full interface

--- include/ifca/ifca.h
+++ include/ifca/ifca.h
@@ -1,26 +1,49 @@
 #pragma once
 
+#include <type_traits>
 #include <future>
 #include <optional>
 
 namespace ifca {
 
-  template<typename T, typename S>
-  using operation = S (*)(T);
+  enum FuncType {
+      Type1T,
+      Type2T,
+  };
 
-  template <typename T, typename S>
+  namespace transform {
+    template<typename T, typename S>
+    using synchronous = S (*)(T);
+
+    template<typename T, typename S>
+    using async_promise = std::promise<S> (*)(T);
+
+    template<typename T, typename S>
+    using async_future = std::future<S> (*)(T);
+  }
+
+  template <typename T, typename S, typename I = void>
   class IFCA {
-    int m_maxParallel;
-    std::array<operation<T,S>, 0>* f_op;
+    static_assert(std::is_base_of<IFCA, I>::value, "I must inherit from IFCA");
 
   public:
-    IFCA(int maxParallel, operation<T,S> *op);
+    IFCA(int maxParallel, transform::synchronous<T,S> *op);
+    IFCA(int maxParallel, transform::async_promise<T,S> *op);
+    IFCA(int maxParallel, transform::async_future<T,S> *op);
 
     std::optional<std::promise<void>> write(T chunk);
     std::optional<std::promise<void>> end();
 
     std::promise<std::optional<S>> read();
 
+    template<typename W>
+    IFCA<T,W,IFCA<T,S,I>> addTransform(transform::synchronous<S,W> *op);
+    template<typename W>
+    IFCA<T,W,IFCA<T,S,I>> addTransform(transform::async_promise<S,W> *op);
+    template<typename W>
+    IFCA<T,W,IFCA<T,S,I>> addTransform(transform::async_future<S,W> *op);
+
+    I removeTransform();
   };
 
 }  // namespace greeter
--- source/ifca.cpp
+++ source/ifca.cpp
@@ -5,8 +5,5 @@ using namespace std;
 
 namespace ifca {
 
-  template <typename T, typename S> IFCA<T,S>::IFCA(int _maxParallel, ifca::operation<T,S>* op)
-    : m_maxParallel{_maxParallel}, f_op{op}
-  {};
 
 }
